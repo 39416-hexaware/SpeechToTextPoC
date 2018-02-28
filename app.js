39c64ab4710939aceb4ffc6fedef116c9ec04214
Arun Prasaath <39402.hexaware@gmail.com>
2018-02-28 11:54:09 +0530

    AP : Experimenting with a wav file with some sample speech to text conversion

--- app.js
+++ app.js
@@ -1,66 +1,188 @@
+// /*-----------------------------------------------------------------------------
+// A simple echo bot for the Microsoft Bot Framework. 
+// -----------------------------------------------------------------------------*/
+
+// var restify = require('restify');
+// var builder = require('botbuilder');
+// var botbuilder_azure = require("botbuilder-azure");
+
+// // Setup Restify Server
+// var server = restify.createServer();
+// server.listen(process.env.port || process.env.PORT || 3978, function () {
+//    console.log('%s listening to %s', server.name, server.url); 
+// });
+  
+// // Create chat connector for communicating with the Bot Framework Service
+// var connector = new builder.ChatConnector({
+//     appId: process.env.MicrosoftAppId,
+//     appPassword: process.env.MicrosoftAppPassword,
+//     openIdMetadata: process.env.BotOpenIdMetadata 
+// });
+
+// // Listen for messages from users 
+// server.post('/api/messages', connector.listen());
+
+// /*----------------------------------------------------------------------------------------
+// * Bot Storage: This is a great spot to register the private state storage for your bot. 
+// * We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
+// * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
+// * ---------------------------------------------------------------------------------------- */
+
+// var tableName = 'botdata';
+// var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
+// var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
+
+// // Create your bot with a function to receive messages from the user
+// var bot = new builder.UniversalBot(connector);
+// bot.set('storage', tableStorage);
+
+// // Make sure you add code to validate these fields
+// var luisAppId = process.env.LuisAppId;
+// var luisAPIKey = process.env.LuisAPIKey;
+// var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+
+// const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+
+// // Main dialog with LUIS
+// var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+// var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+// .matches('Greeting', (session) => {
+//     session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
+// })
+// .matches('Help', (session) => {
+//     session.send('You reached Help intent, you said \'%s\'.', session.message.text);
+// })
+// .matches('Cancel', (session) => {
+//     session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
+// })
+// /*
+// .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+// */
+// .onDefault((session) => {
+//     session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+// });
+
+// bot.dialog('/', intents);  
+
 /*-----------------------------------------------------------------------------
-A simple echo bot for the Microsoft Bot Framework. 
+A speech to text bot for the Microsoft Bot Framework. 
 -----------------------------------------------------------------------------*/
 
-var restify = require('restify');
-var builder = require('botbuilder');
-var botbuilder_azure = require("botbuilder-azure");
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
+
+var builder = require('botbuilder'),
+    fs = require('fs'),
+    needle = require('needle'),
+    restify = require('restify'),
+    request = require('request'),
+    url = require('url'),
+    speechService = require('./speech-service.js');
+
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
-  
-// Create chat connector for communicating with the Bot Framework Service
+
+// Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    openIdMetadata: process.env.BotOpenIdMetadata 
+    appPassword: process.env.MicrosoftAppPassword
 });
 
-// Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot. 
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
-
-var tableName = 'botdata';
-var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
-
-// Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
-bot.set('storage', tableStorage);
-
-// Make sure you add code to validate these fields
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
-
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
-
-// Main dialog with LUIS
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] })
-.matches('Greeting', (session) => {
-    session.send('You reached Greeting intent, you said \'%s\'.', session.message.text);
-})
-.matches('Help', (session) => {
-    session.send('You reached Help intent, you said \'%s\'.', session.message.text);
-})
-.matches('Cancel', (session) => {
-    session.send('You reached Cancel intent, you said \'%s\'.', session.message.text);
-})
-/*
-.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
-*/
-.onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+var bot = new builder.UniversalBot(connector, function (session) {
+    if (hasAudioAttachment(session)) {
+        var stream = getAudioStreamFromMessage(session.message);
+        speechService.getTextFromAudioStream(stream)
+            .then(function (text) {
+                session.send(processText(text));
+            })
+            .catch(function (error) {
+                session.send('Oops! Something went wrong. Try again later.');
+                console.error(error);
+            });
+    } else {
+        session.send('Did you upload an audio file? I\'m more of an audible person. Try sending me a wav file');
+    }
 });
 
-bot.dialog('/', intents);    
+//=========================================================
+// Utilities
+//=========================================================
+function hasAudioAttachment(session) {
+    return session.message.attachments.length > 0 &&
+        (session.message.attachments[0].contentType === 'audio/wav' ||
+            session.message.attachments[0].contentType === 'application/octet-stream');
+}
+
+function getAudioStreamFromMessage(message) {
+    var headers = {};
+    var attachment = message.attachments[0];
+    if (checkRequiresToken(message)) {
+        // The Skype attachment URLs are secured by JwtToken,
+        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+        // https://github.com/Microsoft/BotBuilder/issues/662
+        connector.getAccessToken(function (error, token) {
+            var tok = token;
+            headers['Authorization'] = 'Bearer ' + token;
+            headers['Content-Type'] = 'application/octet-stream';
+
+            return needle.get(attachment.contentUrl, { headers: headers });
+        });
+    }
+
+    headers['Content-Type'] = attachment.contentType;
+    return needle.get(attachment.contentUrl, { headers: headers });
+}
+
+function checkRequiresToken(message) {
+    return message.source === 'skype' || message.source === 'msteams';
+}
+
+function processText(text) {
+    var result = 'You said: ' + text + '.';
+
+    if (text && text.length > 0) {
+        var wordCount = text.split(' ').filter(function (x) { return x; }).length;
+        result += '\n\nWord Count: ' + wordCount;
+
+        var characterCount = text.replace(/ /g, '').length;
+        result += '\n\nCharacter Count: ' + characterCount;
+
+        var spaceCount = text.split(' ').length - 1;
+        result += '\n\nSpace Count: ' + spaceCount;
+
+        var m = text.match(/[aeiou]/gi);
+        var vowelCount = m === null ? 0 : m.length;
+        result += '\n\nVowel Count: ' + vowelCount;
+    }
+
+    return result;
+}
+
+//=========================================================
+// Bots Events
+//=========================================================
+
+// Sends greeting message when the bot is first added to a conversation
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+                var reply = new builder.Message()
+                    .address(message.address)
+                    .text('Hi! I am SpeechToText Bot. I can understand the content of any audio and convert it to text. Try sending me a wav file.');
+                bot.send(reply);
+            }
+        });
+    }
+});
+  
 
